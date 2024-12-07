1499050719dc245dd4127d7a66563f4603e6ebfe
AhmedAlsaiem <ahmedalsaiem2025@gmail.com>
2024-12-07 14:32:47 +0200

    create feature authentication  structure and views

---
+++ lib/feature/authentaction/presentation/view/verfy_acount.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class VerfyAcountView extends StatelessWidget {
+  const VerfyAcountView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
