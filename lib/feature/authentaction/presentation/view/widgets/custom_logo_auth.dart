81ae4e0308385aed73184cc0c393e2f49aa9ea2c
AhmedAlsaiem <ahmedalsaiem2025@gmail.com>
2025-01-17 13:44:35 +0200

    update some data

--- lib/feature/authentaction/presentation/view/widgets/custom_logo_auth.dart
+++ lib/feature/authentaction/presentation/view/widgets/custom_logo_auth.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:splash_app/core/helper/extentions.dart';
 import 'package:splash_app/core/utils/app_size.dart';
 import 'package:splash_app/core/utils/assets_manager.dart';
 import 'package:splash_app/core/utils/color_manager.dart';
@@ -7,9 +6,11 @@ import 'package:splash_app/core/utils/styles_manager.dart';
 
 class CustomLogoAuth extends StatelessWidget {
   final String title;
+  final double height;
   const CustomLogoAuth({
     super.key,
     required this.title,
+    required this.height,
   });
 
   @override
@@ -20,7 +21,7 @@ class CustomLogoAuth extends StatelessWidget {
           borderRadius:
               BorderRadius.vertical(bottom: Radius.circular(AppSize.s28))),
       width: AppSize.infinty,
-      height: context.screenHeight * 0.3,
+      height: height,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
@@ -31,7 +32,7 @@ class CustomLogoAuth extends StatelessWidget {
           const SizedBox(height: AppSize.s14),
           Text(
             title,
-            style: StylesManager.textStyleExtraBold32.copyWith(
+            style: StylesManager.textStyleBold24.copyWith(
               color: ColorsManager.white,
             ),
           )
